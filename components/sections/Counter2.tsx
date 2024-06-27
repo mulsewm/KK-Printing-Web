e9c6413d06e7ab66bb565f7b9b9efff2a457ced3
mulsewm <mulsewm@protonmail.com>
2024-06-27 13:24:32 +0300

    added a counter section

--- components/sections/Counter2.tsx
+++ components/sections/Counter2.tsx
@@ -10,8 +10,8 @@ export default function Counter2() {
                                     <div className="counter-box_icon">
                                         <i className="icon-service-icon1" />
                                     </div>
-                                    <h3 className="counter-box_title"><span className="counter-number">26</span>+ Design Awards</h3>
-                                    <p className="counter-box_text">We are a creative agency brands building insightful strategy, creating unique designs helping</p>
+                                    <h3 className="counter-box_title"><span className="counter-number">15</span>+ Years in Business</h3>
+                                    <p className="counter-box_text">Providing top-notch printing services and quality stationery supplies to a wide range of customers.</p>
                                 </div>
                             </div>
                             <div className="col-xl-auto col-lg-4 col-md-6 counter-divider">
@@ -19,8 +19,8 @@ export default function Counter2() {
                                     <div className="counter-box_icon">
                                         <i className="icon-service-icon2" />
                                     </div>
-                                    <h3 className="counter-box_title"><span className="counter-number">247</span>+ Total Projects</h3>
-                                    <p className="counter-box_text">We are a creative agency brands building insightful strategy, creating unique designs helping</p>
+                                    <h3 className="counter-box_title"><span className="counter-number">500</span>+ Completed Projects</h3>
+                                    <p className="counter-box_text">Successfully delivered high-quality printing solutions and stationery materials for various needs.</p>
                                 </div>
                             </div>
                             <div className="col-xl-auto col-lg-4 col-md-6 counter-divider">
@@ -28,8 +28,35 @@ export default function Counter2() {
                                     <div className="counter-box_icon">
                                         <i className="icon-service-icon3" />
                                     </div>
-                                    <h3 className="counter-box_title"><span className="counter-number">140</span>+ Happy Customers</h3>
-                                    <p className="counter-box_text">We are a creative agency brands building insightful strategy, creating unique designs helping</p>
+                                    <h3 className="counter-box_title"><span className="counter-number">300</span>+ Satisfied Customers</h3>
+                                    <p className="counter-box_text">Earning the trust and satisfaction of our customers with reliable services and premium products.</p>
+                                </div>
+                            </div>
+                            <div className="col-xl-auto col-lg-4 col-md-6 counter-divider">
+                                <div className="counter-box">
+                                    <div className="counter-box_icon">
+                                        <i className="icon-service-icon4" />
+                                    </div>
+                                    <h3 className="counter-box_title"><span className="counter-number">1000</span>+ Products Available</h3>
+                                    <p className="counter-box_text">Offering a wide range of printing and stationery products to meet diverse customer needs.</p>
+                                </div>
+                            </div>
+                            <div className="col-xl-auto col-lg-4 col-md-6 counter-divider">
+                                <div className="counter-box">
+                                    <div className="counter-box_icon">
+                                        <i className="icon-service-icon5" />
+                                    </div>
+                                    <h3 className="counter-box_title"><span className="counter-number">50</span>+ Corporate Clients</h3>
+                                    <p className="counter-box_text">Trusted by numerous corporate clients for consistent quality and exceptional service.</p>
+                                </div>
+                            </div>
+                            <div className="col-xl-auto col-lg-4 col-md-6 counter-divider">
+                                <div className="counter-box">
+                                    <div className="counter-box_icon">
+                                        <i className="icon-service-icon6" />
+                                    </div>
+                                    <h3 className="counter-box_title"><span className="counter-number">24/7</span> Support</h3>
+                                    <p className="counter-box_text">Providing round-the-clock support to ensure customer satisfaction and timely service delivery.</p>
                                 </div>
                             </div>
                         </div>
