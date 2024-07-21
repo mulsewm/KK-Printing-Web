38dda85316828f069c9fbfe41b7bf9d7c8d88a4d
mulsewm <mulsewm@protonmail.com>
2024-07-21 16:06:52 +0300

    ..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: { Component: React.ElementType; pageProps: any }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
